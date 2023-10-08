29eb606e41b7be708f85cf69fb60af74d20d63ab
Samiibrahim1 <147333889+Samiibrahim1@users.noreply.github.com>
2023-10-08 20:12:03 +0300

    Add files via upload

---
+++ products.php
@@ -0,0 +1,16 @@
+<?php
+// Define an array of products
+$products = [
+    [
+        'name' => 'Panadol Advance',
+        'description' => '32 x 500mg Tablets',
+        'image' => 'panadoll-removebg-preview.png'
+    ],
+    [
+        'name' => 'Nexium',
+        'description' => '28 x 40mg Tablets',
+        'image' => 'nexium-removebg-preview.png'
+    ],
+    // Add more products here...
+];
+?>
